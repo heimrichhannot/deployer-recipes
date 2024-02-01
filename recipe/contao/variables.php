d9204fa04673e2acc14263acb9a1f131fb715ae9
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2024-02-01 17:04:39 +0100

    fixed .htaccess public path

--- recipe/contao/variables.php
+++ recipe/contao/variables.php
@@ -33,7 +33,7 @@ remove('shared_files', 'config/parameters.yml');
 
 add('shared_files', [
     'config/parameters{{yaml_ext}}',
-    'public/.htaccess',
+    '{{public_path}}/.htaccess',
     'system/config/localconfig.php',
     '.env',
 ]);
